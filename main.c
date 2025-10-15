7a524fb65de387e79ca6476358d0424946b88338
Keila Queiroz Cortes <keilawonka4@gmail.com>
2025-10-15 16:03:59 -0300

    add libft

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: kqueiroz <kqueiroz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/13 14:50:44 by kqueiroz          #+#    #+#             */
-/*   Updated: 2025/10/15 15:49:14 by kqueiroz         ###   ########.fr       */
+/*   Updated: 2025/10/15 16:03:30 by kqueiroz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,9 @@
 
 static t_node	*create_node(int value)
 {
-	t_node	*new_node = malloc(sizeof(t_node));
+	t_node	*new_node;
+
+	new_node = malloc(sizeof(t_node));
 	if (!new_node)
 		return (NULL);
 	new_node->value = value;
@@ -24,10 +26,17 @@ static t_node	*create_node(int value)
 
 static t_stack	*init_stack(void)
 {
-	t_stack	*stack = malloc(sizeof(t_stack));
+	t_stack	*stack;
+
+	stack = malloc(sizeof(t_stack));
 	if (!stack)
 		return (NULL);
 	stack->top = NULL;
 	stack->size = 0;
 	return (stack);
+}
+
+int	main(int argc, char **argv)
+{
+	return (0);
 }
