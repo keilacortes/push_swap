39c88c7c066a2279f7b4bfb66964d0ddbaa3f6ce
Keila Côrtes <keilawonka4@gmail.com>
2025-10-23 16:42:01 -0300

    print stacks

--- main.c
+++ main.c
@@ -36,7 +36,94 @@ static t_stack	*init_stack(void)
 	return (stack);
 }
 
+static void free_split(char **split)
+{
+	int	i;
+
+	i = 0;
+	while (split[i])
+	{
+		free(split[i]);
+		i++;
+	}
+	free(split);
+}
+
+static void	stack_a_one_arg(char *arg, t_stack *stack_a)
+{
+	int		count;
+	char	**numbers;
+	t_node	*new_node;
+
+	numbers = ft_split(arg, ' ');
+	count = 0;
+	while (numbers[count])
+		count++;
+	count -= 1;
+	while (count >= 0)
+	{
+		new_node = create_node(ft_atoi(numbers[count]));
+		new_node->next = stack_a->top;
+		stack_a->top = new_node;
+		stack_a->size++;
+		count--;
+	}
+	free_split(numbers);
+}
+
+static void	stack_a_mult_args(int argc, char **argv, t_stack *stack_a)
+{
+	int		i;
+	t_node	*new_node;
+
+	i = argc - 1;
+	while (i >= 1)
+	{
+		new_node = create_node(ft_atoi(argv[i]));
+		new_node->next = stack_a->top;
+		stack_a->top = new_node;
+		stack_a->size++;
+		i--;
+	}
+}
+
+void print_stack(t_stack *stack, char name)
+{
+    t_node *current;
+
+    if (!stack || !stack->top)
+    {
+        ft_printf("Stack %c: (empty)\n", name);
+        return;
+    }
+    ft_printf("Stack %c: ", name);
+    current = stack->top;
+    while (current)
+    {
+        ft_printf("%d ", current->value);
+        current = current->next;
+    }
+    ft_printf("(size: %d)\n", stack->size);
+}
+
 int	main(int argc, char **argv)
 {
+	t_stack	*stack_a;
+	t_stack	*stack_b;
+
+	if (argc < 2)
+		return (0);
+	stack_a = init_stack();
+	stack_b = init_stack();
+	if (!stack_a || !stack_b)
+		return (1);
+	if (argc == 2)
+		stack_a_one_arg(argv[1], stack_a);
+	else if (argc > 2)
+		stack_a_mult_args(argc, argv, stack_a);
+	print_stack(stack_a, 'A');
+	print_stack(stack_b, 'B');
+	free_stack(stack_a);
+	free_stack(stack_b);
 	return (0);
 }
