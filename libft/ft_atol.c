1dbcf90ae4eca9f0094f5228952769e64da15c77
Keila Côrtes <keilawonka4@gmail.com>
2025-10-24 14:50:24 -0300

    add atol

---
+++ libft/ft_atol.c
@@ -0,0 +1,28 @@
+//header
+
+#include "libft.h"
+
+long	ft_atol(const char *nptr)
+{
+	int	    i;
+	int	    sign;
+	long	nb;
+
+	nb = 0;
+	i = 0;
+	sign = 1;
+	while ((nptr[i] >= 9 && nptr[i] <= 13) || nptr[i] == 32)
+		i++;
+	if (nptr[i] == '+' || nptr[i] == '-')
+	{
+		if (nptr[i] == '-')
+			sign = -1;
+		i++;
+	}
+	while (nptr[i] >= '0' && nptr[i] <= '9')
+	{
+		nb = (nb * 10) + (nptr[i] - '0');
+		i++;
+	}
+	return (nb * sign);
+}
