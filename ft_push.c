45711797f852fe7e5ff77616aa0b4bb351d9fa3b
Keila Côrtes <keilawonka4@gmail.com>
2025-10-27 15:02:59 -0300

    fiz size stack

--- ft_push.c
+++ ft_push.c
@@ -22,6 +22,7 @@ void    pa(t_stack *a, t_stack *b)
     tmp->next = a->top;
     a->top = tmp;
     a->size++;
+    b->size--;
     ft_printf("pa\n");
 }
 
@@ -35,5 +36,6 @@ void    pb(t_stack *a, t_stack *b)
     tmp->next = b->top;
     b->top = tmp;
     b->size++;
+    a->size--;
     ft_printf("pb\n");
 }
